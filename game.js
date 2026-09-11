cecc46ce8f27bc766b5fa8ea5111dd1c55256973
sumin-t <sml30527@gmail.com>
2026-09-11 10:14:11 +0900

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@
    Code.gs를 구글 시트에 배포한 뒤 나오는 "웹 앱 URL"을 붙여넣으면
    명예의 전당 등록/조회 기능이 활성화됩니다.
    ============================================================ */
-const APPS_SCRIPT_URL = "https://docs.google.com/spreadsheets/d/12ydYSPUiDIEuHsbO8buSiWWOSf17Psybon-PmxAZTWA/edit?usp=sharing"; 
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_DztBoGNc5c8IoI0CPT58_-DLMx9YllTS2kFvzpr9BYxiwHMl4QKRSWNVeh9JKrPi8Q/exec"; 
 
 /* ------------------------- 원소 데이터 ------------------------- */
 // 원자번호 1~20
